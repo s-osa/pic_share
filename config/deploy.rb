57ccf4c4efbf6fb97a7dc80dc24e81e90efb82fe
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2013-06-11 01:18:04 +0900

    edit deploy.rb and database.yml .

---
+++ config/deploy.rb
@@ -0,0 +1,22 @@
+#config/deploy.rb
+require "bundler/capistrano"
+load "deploy/assets"
+
+# Git repository
+set :scm,         "git"
+set :repository,  "git@github.com:xxxcaqui/pic_share.git"
+#set :branch,      "master"
+set :ssh_options, { forward_agent: true }
+
+# Server setting
+ec2 = "ec2-54-249-72-143.ap-northeast-1.compute.amazonaws.com"
+role :web, ec2
+role :app, ec2
+role :db,  ec2, :primary => true
+
+set :application, "pic_share"
+set :user, "rails"
+set :use_sudo, false
+set :deploy_to, "/home/#{user}/#{application}"
+set :rails_env, "production"
+default_run_options[:shell] = '/bin/bash --login'
