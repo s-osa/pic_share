9330bf8f6ed739410aac6dc2f0c71b1decc83e81
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2013-06-11 00:50:53 +0900

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PicShare::Application.config.secret_token = '2458edd6fdddc5ebfabd3e045807e291fa9ef8436f1a750e2e18bdf6b7f6480bb7ac505b3d3281069697247bfd15ae5566cfe1ada795d5c5653cd5b0a3d7a7f4'
